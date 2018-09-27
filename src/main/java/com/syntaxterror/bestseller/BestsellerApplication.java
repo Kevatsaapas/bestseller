07275ee5e0eb95237602f7005f0cbb92b788bdd4
Nebularlion <elina.rajala@hotmail.fi>
2018-09-27 11:46:02 +0300

    Commandlinerunner pois
    Commandlinerunner pois

--- src/main/java/com/syntaxterror/bestseller/BestsellerApplication.java
+++ src/main/java/com/syntaxterror/bestseller/BestsellerApplication.java
@@ -1,24 +1,7 @@
 package com.syntaxterror.bestseller;
 
-import java.util.Date;
-
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import com.syntaxterror.bestseller.model.Kilpailija;
-import com.syntaxterror.bestseller.model.Kilpailu;
-import com.syntaxterror.bestseller.model.Lohko;
-import com.syntaxterror.bestseller.model.Ostaja;
-import com.syntaxterror.bestseller.model.Tuomari;
-import com.syntaxterror.bestseller.repository.ArviointiRepository;
-import com.syntaxterror.bestseller.repository.KilpailijaRepository;
-import com.syntaxterror.bestseller.repository.KilpailuRepository;
-import com.syntaxterror.bestseller.repository.KriteeriRepository;
-import com.syntaxterror.bestseller.repository.LohkoRepository;
-import com.syntaxterror.bestseller.repository.OstajaRepository;
-import com.syntaxterror.bestseller.repository.TuomariRepository;
 
 @SpringBootApplication
 public class BestsellerApplication {
