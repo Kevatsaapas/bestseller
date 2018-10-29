e8fb4ec800806321295fe5ee74125fe1b44ab0fa
Nebularlion <elina.rajala@hotmail.fi>
2018-10-29 14:34:27 +0200

    Security aloitettu

---
+++ src/main/java/com/syntaxterror/bestseller/security/DataSource.java
@@ -0,0 +1,11 @@
+package com.syntaxterror.bestseller.security;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+
+/**
+@ConfigurationProperties("spring.datasource.*")
+public class DataSource datasource(){
+
+}
+**/
