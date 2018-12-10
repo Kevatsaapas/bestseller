d09ce628a06a0d805230abc5b116b2c295b12cb1
Nebularlion <elina.rajala@hotmail.fi>
2018-12-10 12:55:57 +0200

    Some updates

--- src/main/java/com/syntaxterror/bestseller/BestsellerApplication.java
+++ src/main/java/com/syntaxterror/bestseller/BestsellerApplication.java
@@ -2,18 +2,11 @@ package com.syntaxterror.bestseller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BestsellerApplication extends SpringBootServletInitializer {
+public class BestsellerApplication {
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(BestsellerApplication.class);
-	}
-
-	public static void main(String[] args)throws Exception {
+	public static void main(String[] args) {
 		SpringApplication.run(BestsellerApplication.class, args);
 
 	}
