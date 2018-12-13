ec21479441dda0e737c8645167caa29fab46c6a8
Nirkko Mikko <a1503030@myy.haaga-helia.fi>
2018-12-13 11:50:15 +0200

    yleisvaikutelma

--- src/main/resources/templates/LocalStorage.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    if(window.localStorage)
-        console.log("Local Storage supported")
-    else
-        console.log("Local Storage not supported")
-
-})();
-
-// document.getElementById(kilpailijaId).innerHTML = localStorage.setItem(kilpailijaId, kilpailija);
